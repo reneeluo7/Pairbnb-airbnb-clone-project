691844c22aa1def61ac090eea72ed94cd8e1b736
reneeluo7 <rong_2_u@hotmail.com>
2022-06-30 09:07:45 -0700

    first commit

---
+++ backend/routes/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+// import apirouter connect it to the router
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
+
+
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
+});
+
+
+
+
+
+
+module.exports = router;
