baf54599299b874be633ccfe148dfc00cf5efe41
reneeluo7 <rong_2_u@hotmail.com>
2022-07-08 09:46:56 -0700

    create booking

--- backend/db/seeders/20220707203428-demo-booking.js
+++ backend/db/seeders/20220707203428-demo-booking.js
@@ -7,32 +7,32 @@ module.exports = {
     {
         spotId: 1,
         userId: 2,
-        startDate: new Date("2022-09-30"), 
-        endDate: new Date("2022-09-30")
+        startDate: "2022-09-30", 
+        endDate: "2022-09-30"
       },
     {
       spotId: 1,
       userId: 2,
-      startDate: new Date("2022-10-03"),
-      endDate: new Date("2022-10-07")
+      startDate: "2022-10-03",
+      endDate: "2022-10-07"
       },
     {
       spotId: 3,
       userId: 1,
-      startDate: new Date("2022-08-30"), 
-      endDate: new Date("2022-09-02")
+      startDate: "2022-08-30", 
+      endDate: "2022-09-02"
       },
     {
       spotId: 2,
       userId: 3,
-      startDate: new Date("2022-08-01"), 
-      endDate: new Date("2022-08-13")
+      startDate: "2022-08-01", 
+      endDate: "2022-08-13"
       },
     {
       spotId: 2,
       userId: 2,
-      startDate: new Date("2022-11-01"), 
-      endDate: new Date("2022-11-10")
+      startDate: "2022-11-01", 
+      endDate: "2022-11-10"
       }
     ], {});
   
