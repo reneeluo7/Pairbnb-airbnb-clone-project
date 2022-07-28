c094d71dc9fa4d21634a555a681915e1a012a4f3
reneeluo7 <rong_2_u@hotmail.com>
2022-07-28 16:19:56 -0700

    workingoncss

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -37,7 +37,7 @@ function ProfileButton({ user }) {
   return (
     <>
       <button onClick={openMenu}>
-        <i className="fas fa-user-circle" />
+        <i className="fas fa-user-circle fa-2x" />
       </button>
       {showMenu && (
         <ul className="profile-dropdown">
