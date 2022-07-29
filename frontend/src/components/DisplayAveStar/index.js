331db9230e6c74b42fadb1feef83faab8f2be6b2
reneeluo7 <rong_2_u@hotmail.com>
2022-07-29 10:43:43 -0700

    testing ave star

--- frontend/src/components/DisplayAveStar/index.js
+++ frontend/src/components/DisplayAveStar/index.js
@@ -1,20 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import React from 'react';
-import { getSpotsReviews } from '../../store/reviews'
+// import { getOneSpot } from '../../store/spots';
+import{ getSpotsReviews } from '../../store/reviews';
 
-function DisplayAveStar({spotId}) {
-    const dispatch = useDispatch();
-    const reviews = useSelector(state => Object.values(state.reviews));
-    const spotReviews = reviews.filter(review => review.spotId === Number(spotId));
-    const avgStarRating = spotReviews.map(el => el.stars).reduce((a, b) => a + b, 0) / spotReviews.length;
+function DisplayAveStar({ spotId }) {
+  const dispatch = useDispatch();
+  const reviews = useSelector(state => Object.values(state.reviews));
+  const spotReviews = reviews.filter(review => review.spotId === Number(spotId));
+  const avgStarRating = spotReviews.map(el => el.stars).reduce((a, b) => a + b, 0) / spotReviews.length;
+  // const spot = useSelector(state => state.spots[spotId]);
+  // const [isLoaded, setIsLoaded] = useState(false);
+  console.log("reviews======", reviews)
 
-    useEffect(() => {
-        dispatch(getSpotsReviews(spotId))
-    }, [dispatch, spotId]);
+  useEffect(() => {
+    dispatch(getSpotsReviews(spotId))
+  }, [dispatch, spotId]);
 
   return (
-     <span key={spotId}>{avgStarRating ? (`${avgStarRating.toFixed(2)}`) : (`New`) }</span>
+    <>
+      
+        <span key={spotId}>{avgStarRating ? (`${avgStarRating.toFixed(2)}`) : (`New`)}</span>
+      
+    </>
   )
 }
 
