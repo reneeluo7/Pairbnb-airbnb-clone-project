0960de76b207c20f0d1217be98d82c91e65567ba
reneeluo7 <rong_2_u@hotmail.com>
2022-07-29 13:51:46 -0700

    workingoncss

--- frontend/src/components/SpotReviewList/index.js
+++ frontend/src/components/SpotReviewList/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getSpotsReviews } from '../../store/reviews';
+
 // import ReviewFormModal from '../ReviewFormModal';
 
 
